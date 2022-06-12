0183f09e94927374c056ab2b5ff267d056033a09
fernandoprestes <fernandoprestesc@gmail.com>
2022-06-12 13:35:10 -0300

    refactor: refatora a forma de adicionar class

--- components/Atoms/Badge.vue
+++ components/Atoms/Badge.vue
@@ -14,14 +14,14 @@
   });
   const classNames = computed(() => {
     return {
-      [props.variant]: true,
-      [props.color]: true,
+      [`bg-${props.color}/90 text-white`]: props.variant === 'solid',
+      [`bg-${props.color}/20 text-${props.color}`]: props.variant === 'soft',
     };
   });
 </script>
 <template>
   <span
-    class="wrapper"
+    class="inline-flex items-center gap-2 py-1 px-2 rounded-full text-xs font-semibold"
     v-bind="$attrs"
     :class="classNames"
   >
@@ -31,56 +31,4 @@
 <style
   scoped
   lang="scss"
->
-  .wrapper {
-    @apply inline-flex
-            items-center
-            gap-2
-            py-1
-            px-2
-            rounded-full
-            text-xs
-            font-semibold;
-  }
-  .solid {
-    @apply text-white;
-    &.primary {
-      @apply bg-primary;
-    }
-    &.secondary {
-      @apply bg-secondary;
-    }
-    &.info {
-      @apply bg-info;
-    }
-    &.success {
-      @apply bg-success;
-    }
-    &.warning {
-      @apply bg-warning;
-    }
-    &.error {
-      @apply bg-error;
-    }
-  }
-  .soft {
-    &.primary {
-      @apply bg-primary/20 text-primary;
-    }
-    &.secondary {
-      @apply bg-secondary/20 text-secondary;
-    }
-    &.info {
-      @apply bg-info/20 text-info;
-    }
-    &.success {
-      @apply bg-success/20 text-success;
-    }
-    &.warning {
-      @apply bg-warning/20 text-warning;
-    }
-    &.error {
-      @apply bg-error/20 text-error;
-    }
-  }
-</style>
+></style>
